553935c30d581c66f368fafddaaa929598b1ef9a
ali-0315 <80034535+ali-0315@users.noreply.github.com>
2022-01-23 19:39:54 +0330

    fix password and complet the login page

--- Jscript1.js
+++ Jscript1.js
@@ -1,9 +1,9 @@
 function ch1() {
     email = String(document.getElementById("em1").value);
     pass = String(document.getElementById("ps1").value);
-    if (email == "as7490226@gmail.com" || email == "ali0315") {
-        window.alert("شما با موفقیت وارد شدید!")
+    if (email == "as7490226@gmail.com" || email == "ali0315" && pass == "ali0315") {
+        window.alert("شما با موفقیت وارد شدید !")
     } else {
-        window.alert("نام کاربری یا ایمیل با این مشخصات وجود ندارد!")
+        window.alert("نام کاربری یا رمزعبور اشتباه است !")
     }
 }
